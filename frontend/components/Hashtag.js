05140116918e210538828ae0de3e59cb3f411dcb
Sandrine <fialon.sandrine@gmail.com>
2023-04-27 18:41:57 +0200

    Save hashtag

--- frontend/components/Hashtag.js
+++ frontend/components/Hashtag.js
@@ -4,8 +4,25 @@ import Tweet from './Tweet';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEgg } from '@fortawesome/free-solid-svg-icons'
+import { useState, useEffect } from 'react';
 
 function Hashtag() {
+  let trends = []
+  //useEffect()  
+  fetch('https://hackatweet-backend-ac9g.vercel.app/hashtags')
+  .then(response => response.json())  
+  .then(
+    data => {
+      if(data) {
+        console.log(data);
+        const listHash = data.hashtags
+        listHash.map( hash => {  
+          trends.push(<Trends name={hash}/>)
+        } )
+     }
+    }
+  ) 
+  
   return (
     <div className={styles.container}>
       <div className={styles.logoLeft}>
@@ -28,7 +45,7 @@ function Hashtag() {
       </div>
       <div className={styles.borderLeft}>
         <h2 className={styles.titreNoBold}>Trends</h2>
-        <Trends/>
+        {trends}
       </div>
     </div>
   );
